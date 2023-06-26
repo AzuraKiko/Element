1af71797e159566e0f679addb84454b536fa5eb7
Khánh Trần <tranhuyen1198@gmail.com>
2023-06-26 13:42:17 +0700

    Auto stash before merge of "master" and "origin/master"

---
+++ demo.js
@@ -0,0 +1,13 @@
+// console.log (a)
+// var a = 10
+
+
+// function test() {
+//     console.log (a)
+
+//     let a = 10 
+// }
+
+const object = { name : 'A', age : 33, }  
+function test(){ for (const test in object) { console.log (test) }  }
+test()
