b642704cae45ece91e39f7e2629af1f570cbf7e1
Khánh Trần <tranhuyen1198@gmail.com>
2023-06-26 17:12:04 +0700

    ver4

--- demo.js
+++ demo.js
@@ -8,6 +8,8 @@
 //     let a = 10 
 // }
 
-const object = { name : 'A', age : 33, }  
-function test(){ for (const test in object) { console.log (test) }  }
+const object = { name: 'A', age: 33, }
+function test() { for (const test in object) { console.log(test) } }
 test()
+
+
